9abc0a724d306956f387c823d6ffb92a69362c6b
Dongwoo Yu <dongwoozzik@naver.com>
2021-12-08 21:48:08 +0900

    1208_momentum_클론코딩_01
    노마드코더 momentum 클론 코딩 (날씨 api제외)

---
+++ js/background.js
@@ -0,0 +1,21 @@
+const images = [
+    "01.jpg",
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg",
+    "06.jpg",
+    "07.jpg",
+    "08.jpg",
+    "09.jpg",
+    "10.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImages = document.createElement("img");
+
+bgImages.src = `./img/${chosenImage}`;
+
+document.body.appendChild(bgImages);
